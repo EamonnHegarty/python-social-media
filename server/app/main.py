54e32c9fc7059423991b11dedcc7414267c1b117
Eamonn Hegarty <eamonnhegartyy@hotmail.co.uk>
2023-12-06 09:40:44 +0000

    added posts with comments routes and functions

--- server/app/main.py
+++ server/app/main.py
@@ -1,12 +1,10 @@
+from fastapi import FastAPI
 
 from app.routers.post import router as post_router
-from fastapi import FastAPI
 
 # FROM root so you can see client & server
 # uvicorn app.main:app --reload
-# source venv/Scripts/activate
+# source .venv/Scripts/activate
 
 app = FastAPI()
 app.include_router(post_router)
-
-
