c4ae116ba7e288a445f48d626fb8fd05021b908f
Eamonn Hegarty <eamonnhegartyy@hotmail.co.uk>
2023-12-06 12:00:48 +0000

    added comments tests

--- server/app/main.py
+++ server/app/main.py
@@ -1,8 +1,7 @@
+from app.routers.post import router as post_router
 from fastapi import FastAPI
 
-from app.routers.post import router as post_router
-
-# FROM root so you can see client & server
+# FROM /server
 # uvicorn app.main:app --reload
 # source .venv/Scripts/activate
 
